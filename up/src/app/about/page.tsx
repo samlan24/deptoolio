1e8059a2404ce613bb698da769addc18a4c73b50
samlan24 <muchemionline@gmail.com>
2025-09-02 15:23:21 +0300

    making changes

--- up/src/app/about/page.tsx
+++ up/src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function AboutPage() {
   return (
-    <div className="max-w-3xl mx-auto px-6 py-12">
+    <div className="pt-20 max-w-3xl mx-auto px-6 py-12">
       <h1 className="text-3xl font-bold mb-6">About Pacgie</h1>
 
       <p className="mb-6">
