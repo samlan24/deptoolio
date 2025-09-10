519a3fe626b51c98246ce626ea52f23152d9c6c9
samlan24 <muchemionline@gmail.com>
2025-09-10 18:00:32 +0300

    fix(up): remove /guides from robots.txt allow list

--- up/src/app/robots.ts
+++ up/src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/', '/about', '/terms', '/privacy', '/guides', '/login'],
+      allow: ['/', '/about', '/terms', '/privacy', '/login'],
       disallow: ['/api/', '/dashboard', '/checks','/repo-scanner'],
     },
     sitemap: 'https://www.pacgie.com/sitemap.xml', // Note: using www since that's your canonical
