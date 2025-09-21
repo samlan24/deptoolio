32e4a0d62f99c1b5c59d8f839f6558837f1c4791
samlan24 <muchemionline@gmail.com>
2025-09-21 20:00:46 +0300

    chore(up): add hotjar script to layout for analytics

--- up/src/app/layout.tsx
+++ up/src/app/layout.tsx
@@ -144,6 +144,19 @@ export default function RootLayout({
             gtag('config', 'G-CPB8N6RKJ2');
           `}
         </Script>
+
+        <Script id="hotjar-script">
+          {`
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:6449044,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `}
+        </Script>
       </head>
       <body className={`${inter.variable} antialiased`}>
         <DynamicCanonical />
