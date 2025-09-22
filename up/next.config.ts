b217374a9709b6f3f9dacd23971e06edc8b2b89d
samlan24 <muchemionline@gmail.com>
2025-09-22 04:27:38 +0300

    fix(up): update CSP and image config for external resources\n\nThe Content Security Policy was updated to allow necessary scripts\nand frames from Google Ads and related services. Additionally, the\nimage configuration was updated to use remotePatterns instead of\ndomains

--- up/next.config.ts
+++ up/next.config.ts
@@ -17,13 +17,13 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
             default-src 'self';
-            script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://script.hotjar.com;
+            script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://script.hotjar.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.google.com https://tpc.googlesyndication.com;
             style-src 'self' 'unsafe-inline' https://static.hotjar.com;
             img-src 'self' data: https: https://www.google-analytics.com https://script.hotjar.com;
             font-src 'self' data: https://static.hotjar.com;
             connect-src 'self' https: https://www.google-analytics.com https://region1.google-analytics.com https://*.hotjar.com https://*.hotjar.io wss://*.hotjar.com data: blob:;
             frame-ancestors 'self';
-            frame-src https://vars.hotjar.com;
+            frame-src https://vars.hotjar.com https://googleads.g.doubleclick.net https://tpc.googlesyndication.com;
           `.replace(/\s{2,}/g, " "),
           },
           { key: "X-Frame-Options", value: "SAMEORIGIN" },
@@ -39,7 +39,12 @@ const nextConfig: NextConfig = {
     ];
   },
   images: {
-    domains: ["images.prismic.io"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+      },
+    ],
   },
 };
 
