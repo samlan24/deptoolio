6195735c68b37f5879f70a3e38cf04aa6f954bdf
samlan24 <muchemionline@gmail.com>
2025-10-12 17:15:42 +0300

    adding status

--- up/src/app/components/Footer.tsx
+++ up/src/app/components/Footer.tsx
@@ -127,9 +127,11 @@ const Footer = () => {
                   <Twitter className="w-5 h-5" />
                 </a>
               </div>
-              <a href="https://site-watch-lite.vercel.app/status/7YooF7vjsThOMLl1L5vr" target="_blank" rel="noopener noreferrer">
-  <img src="https://site-watch-lite.vercel.app/api/badge/7YooF7vjsThOMLl1L5vr" alt="Website Status" />
-</a>
+
+              <img
+                src="https://site-watch-lite.vercel.app/api/badge/7YooF7vjsThOMLl1L5vr"
+                alt="Website Status"
+              />
             </div>
           </div>
         </div>
